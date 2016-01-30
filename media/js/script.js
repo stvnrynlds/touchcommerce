7b79ca40fddfc03f7441dcf7c29125c9f35a82ff
Steven Reynolds <steven@stevenreynolds.me>
2016-01-30 12:21:10 -0800

    Added validation placeholder

--- media/js/script.js
+++ media/js/script.js
@@ -1,1 +1,28 @@
-script.js
+// Form Validation
+
+function validateForm() {
+	var form = document.forms["prechatSurvey"],
+		formName = form["name"].value,
+		formPhone = form["phone"].value;
+
+    if (!validName(formName)) {
+        alert("Name must be filled out");
+    }
+
+    if (!validPhone(formPhone)) {
+    	alert("Phone must be filled out");
+    }
+}
+
+function validName(value) {
+	value = value;
+    return false;
+}
+
+function validPhone(value) {
+	value = value;
+	return false;
+}
+
+
+// Panel Transitions
