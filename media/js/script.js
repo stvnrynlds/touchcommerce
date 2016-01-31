f9335a393a8ecb09c0d26718ae72f95bc77a5a81
Steven Reynolds <steven@stevenreynolds.me>
2016-01-30 17:19:04 -0800

    Added conditional field

--- media/js/script.js
+++ media/js/script.js
@@ -9,6 +9,7 @@ var popup = document.getElementById("popupWrap"),
 // Form Validation
 
 function validateForm() {
+
 	var form = document.forms["prechatSurvey"],
 		formName = form["name"].value,
 		formPhone = document.getElementById("phone").value,
@@ -18,14 +19,15 @@ function validateForm() {
 		nameError = "",
 		nameFlag, phoneFlag;
 
-
 	// Check if Full Name is Valid
     if (formName === null || formName === "") {
+    	// Field is empty
     	nameFieldset.className = "is-required has-error";
     	nameError = "Full Name is a required field."
     	nameFlag = false;
     	console.log("Invalid name");
     } else {
+    	// Field has some characters
     	nameFieldset.className = "is-required";
     	nameFlag = true;
     	console.log("Valid name");
@@ -33,35 +35,58 @@ function validateForm() {
 
 	// Check if Phone is Valid
     if (formPhone === null || formPhone === "") {
+    	// Field is empty
     	phoneFieldset.className = "is-required has-error";
 		phoneError = "Mobile number is a required field.";
     	phoneFlag = false;
     } else if (formPhone.replace(/\D/g, '').length != 10) {
+    	// Field does not have 10 numbers
     	phoneFieldset.className = "is-required has-error";
 		phoneError = "Please enter a valid phone number.";
 		phoneFlag = false;
     } else {
+    	// Field has 10 numbers
     	phoneFieldset.className = "is-required";
     	phoneFlag = true;
     }
 
-    // Print errors if there are any
+    // Display errors if there are any
     document.getElementById("nameError").innerHTML = nameError;
     document.getElementById("phoneError").innerHTML = phoneError;
 
     // If everything's valid, show the success screen
     if (nameFlag && phoneFlag) { successForm(); }
+
 }
+
+
+// Conditional Form Element
+
+function stateSelector() {
+	var stateDropDown = document.getElementById("state"),
+		selection = stateDropDown.options[stateDropDown.selectedIndex].value;
+
+		if (selection === "CA") {
+			// If California is selected
+			document.getElementById("hiddenField").className = " ";
+		} else {
+			// If anything else is selected
+			document.getElementById("hiddenField").className = "hidden";
+		}
+}
+
 
 // Panel Transitions
 
 billingBtn.addEventListener("click", function(){ 
+	// move to panel 2 and increase element height
 	panelGuide.className = "panel prev";
 	panelSurvey.className = "panel current";
 	popup.className = "popup popup--tall";
 });
 
 function successForm() {
+	// move to final panel and decrease element height
 	panelSurvey.className = "panel prev";
 	panelCongrats.className = "panel current";
 	popup.className = "popup";
